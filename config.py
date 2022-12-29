52a7a5e2621a1876432473b90b6ac6123c984140
Own Kingdom <113741344+Guru098@users.noreply.github.com>
2022-12-29 20:55:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/56d1760224589e
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://telegra.ph/file/56d1760224589ee370186.jpg",
+    "https://telegra.ph/file/",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
